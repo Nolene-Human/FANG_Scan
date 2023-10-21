ecb520f1956f07904e93174fae7e10e075ce579c
Nolene Human <nolenehuman@hotmail.com>
2023-10-21 16:36:47 +1300

    setup

--- app.py
+++ app.py
@@ -4,31 +4,21 @@ import sqlite3
 
 app = Flask(__name__)
 
+database={'nina@gmail.com':'123'}    
 
 @app.route('/')
 def index():
     return render_template('login.html')
 
-
-database={'nina@gmail.com':'123'}
-    
+# if user exist direct to info_gather else deny access
 @app.route('/form_login',methods=['POST','GET'])
 def login():
     name1=request.form["username"]
     
     if name1 in database:
-        return render_template('pre_engagement.html')
-    else:
-        return "Access Denied"
-
-@app.route('/pre-engagement',methods=['POST','GET'])
-def pre_eng():
-    sign=request.form["name"]
-
-    if sign == "nina":
         return render_template('info_gather.html')
     else:
-        return render_template('pre_engagement.html')
+        return "Access Denied"
 
 
 @app.route('/Intelligence-Gathering')
@@ -37,4 +27,4 @@ def intel():
     return render_template('info_gather.html')
     
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0')
