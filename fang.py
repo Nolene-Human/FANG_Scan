2dc1005ff5dc25f75ff8ed6b29e2f8e488982483
Nolene Human <108449986+Nolene-Human@users.noreply.github.com>
2023-11-15 20:37:18 +1300

    Update fang.py

--- fang.py
+++ fang.py
@@ -8,5 +8,26 @@ app = Flask(__name__)
 def index():
     return render_template('landing.html')
 
-#if __name__=="__main__":
- #   app.run(host='0.0.0.0')
+@app.route('/form_login',methods=['POST','GET'])
+def login():
+
+    if request.method =='POST':
+        conn=sqlite3.connect('database/clients.db')
+        c= conn.cursor()
+        
+        name=request.form["username"]
+        password=request.form["password"]
+        otp=request.form["otp"]
+    
+
+        validate="SELECT username,pass,otp FROM clients where username='"+name+"'and pass ='"+password+"'and otp ='"+otp+"'" 
+        c.execute(validate)
+
+        result=c.fetchall()
+
+
+    #validate 
+        if len(result)==0:
+            return("You are not authorised to enter the site with credentials provided")
+        else:
+            return render_template('intel_gather.html')
