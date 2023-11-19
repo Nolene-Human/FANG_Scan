3e3bfd190688eea4b9f347eec41d0a12dde2f13d
Nolene Human <108449986+Nolene-Human@users.noreply.github.com>
2023-11-19 19:12:32 +1300

    Update fang.py

--- fang.py
+++ fang.py
@@ -1,27 +1,57 @@
 
 from flask import Flask, render_template
 
+# WTF manage form entry validations
+
+
+#from email.mime.application import MIMEApplication
+
+#Security features 
+import nmap
+import pyfile.otp
+import pyfile.password_check
+
+
 app = Flask(__name__)  
+
+
+#Security headers
+@app.after_request
+def security_headers(resp):
+    # preventing man-in-the-middle (MITM) attack
+    resp.headers['Strict-Transport-Security'] = 'max-age=31536000'
+
+    resp.headers['Content-Security-Policy'] = 'default-src "self" https://cdn.jsdelivr.net/npm/water.css@2/out/water.css'
+    # prevent cross-site scripting (XSS) attack.
+    resp.headers['X-Content-Type-Options'] = 'nosniff'
+    # prevents external sites from embedding your site in an iframe (clickjacking)
+    resp.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    return resp
+
+
+#connection to database
+import sqlite3
+import database.database
+
+db_path='database\clients.db'
+
+
 
 @app.route('/')
 def index():
     return render_template('landing.html')
 
+# if user exist direct to info_gather else deny access
 @app.route('/form_login',methods=['POST','GET'])
 def login():
-
-    if request.method =='POST':
-        conn=sqlite3.connect('database/clients.db')
-        c= conn.cursor()
-        
+        conn=sqlite3.connect('clients.db')
+        c=conn.cursor()
         name=request.form["username"]
         password=request.form["password"]
         otp=request.form["otp"]
     
-
-        validate="SELECT username,pass,otp FROM clients where username='"+name+"'and pass ='"+password+"'and otp ='"+otp+"'" 
-        c.execute(validate)
-
+        step1="SELECT username,pass,otp FROM clients where username='"+name+"'and key ='"+password+"'and otp ='"+otp+"'"
+        c.execute(step1)
         result=c.fetchall()
 
 
@@ -29,4 +59,14 @@ def login():
         if len(result)==0:
             return("You are not authorised to enter the site with credentials provided")
         else:
-            return render_template('intel_gather.html')
+            conn.close()
+            return render_template('landing.html')
+            
+
+
+@app.route('/first',methods=['POST','GET'])
+def verify():
+    key=pyfile.otp.key()
+    pyfile.otp.generate_qr(key)
+
+    return render_template('otp.html')
